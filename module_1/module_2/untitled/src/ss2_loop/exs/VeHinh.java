52d76c1cebd3109976958050d1175b92dfed7699
Bảo Lê <levannhatbao29@gmail.com>
2023-10-10 10:26:41 +0700

    cap nhat

---
+++ module_1/module_2/untitled/src/ss2_loop/exs/VeHinh.java
@@ -0,0 +1,18 @@
+package ss2_loop.exs;
+
+import java.util.Scanner;
+
+public class VeHinh {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Print the rectangle");
+        System.out.println("2. Print the square triangle");
+        System.out.println("3. Print isosceles triangle");
+        System.out.println("4. Exit");
+        int numbers = scanner.nextInt();
+        switch (numbers){
+            case 1:
+                for ()
+        }
+    }
+}
--- module_1/module_2/untitled/src/ss2_loop/pratice/SoNguyenTo.java
+++ module_1/module_2/untitled/src/ss2_loop/pratice/SoNguyenTo.java
@@ -1,2 +1,22 @@
+package ss2_loop.pratice;
+
+import java.util.Scanner;
+
 public class SoNguyenTo {
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số muốn kiểm tra");
+        int numbers = scanner.nextInt();
+        for (int i = 1; i <= numbers; i++) {
+            if (numbers % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.println(numbers+" Là số nguyên tố");
+        }else {
+            System.out.println(numbers+" Không phải là số nguyên tốt");
+        }
+    }
 }
